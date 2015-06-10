6b894c8e5b26441c7eaacf9e2eeb83d04692e7c4
Gustav Hammar <gustav9797@hotmail.se>
2015-06-10 16:11:23 +0200

    Proper master

--- src/main/java/com/github/cheesesoftware/Barrels/Barrel.java
+++ src/main/java/com/github/cheesesoftware/Barrels/Barrel.java
@@ -1,12 +1,9 @@
 package com.github.cheesesoftware.Barrels;
 
-import java.util.ArrayList;
 import java.util.UUID;
 
 import org.bukkit.Location;
 import org.bukkit.inventory.ItemStack;
-
-import com.gmail.filoghost.holographicdisplays.api.Hologram;
 
 public class Barrel {
     private int ID;
@@ -16,24 +13,12 @@ public class Barrel {
     private ItemStack item;
     private int itemAmount;
 
-    private ArrayList<Hologram> holograms = new ArrayList<Hologram>();
-
     public Barrel(int ID, Location location, UUID creatorUUID, ItemStack item, int itemAmount) {
 	this.ID = ID;
 	this.location = location;
 	this.creatorUUID = creatorUUID;
 	this.item = item;
 	this.itemAmount = itemAmount;
-    }
-
-    public void addHologram(Hologram h) {
-	holograms.add(h);
-    }
-
-    public void killHolograms() {
-	for (Hologram h : this.holograms) {
-	    h.delete();
-	}
     }
 
     public int getID() {
